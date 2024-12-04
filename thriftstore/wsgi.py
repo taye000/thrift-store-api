818fc86d5337ffba979740996a99ea173997365e
taye000 <taylorgitari@gmail.com>
2024-12-04 18:02:47 +0300

    set up

---
+++ thriftstore/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for thriftstore project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thriftstore.settings')
+
+application = get_wsgi_application()
